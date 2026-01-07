32027c0b347f9bab479127c389b67fa44d80a229
Vivek Maurya <vivek.maurya.ent@wiom.in>
2026-01-07 11:23:22 +0530

    Add Netlify Flutter build setup

---
+++ netlify_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "📦 Installing Flutter..."
+
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter --version
+flutter doctor
+
+echo "🌐 Enabling Flutter Web..."
+flutter config --enable-web
+
+echo "📦 Getting dependencies..."
+flutter pub get
+
+echo "🏗 Building Flutter Web..."
+flutter build web --release --web-renderer canvaskit
